dab51b079b1b8ba375ea1aab2227855777bcd468
Sima <sima.boreiri@gmail.com>
2020-07-17 13:15:26 +0200

    writes test for request

---
+++ cypress/integration/subscriber_can_add_movie_to_watchlist.feature.js
@@ -0,0 +1,47 @@
+describe("subscriber can add movie to their watchlist ", () => {
+  before(() => {
+    cy.server();
+    cy.route({
+      method: "GET",
+      url: "http://localhost:3000/api/v1/movies/random",
+      response: "fixture:random_movie.json",
+    });
+    cy.visit("/");
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/auth",
+      response: "fixture:login_response.json",
+      headers: {
+        uid: "user@mail.com"
+      }
+    })
+    cy.get("#login").click()
+    cy.get("#login-form").within(() => {
+      cy.get("#email").type("user@mail.com")
+      cy.get("#password").type("password")
+      cy.get("button").contains("Submit").click()
+    })
+    cy.route({
+      method: "POST",
+      url: "http://localhost:3000/api/v1/watchlist",
+      response: "fixture:watchlist_post_response.json"
+    })
+    cy.route({
+      method: "PUT",
+      url: "http://localhost:3000/api/v1/watchlist/1",
+      response: "fixture:watchlist_put_response.json"
+    })
+    it("subscriber can add movie to the watchlist ", () => {
+      cy.get("movie-1").within(() => {
+        cy.get("button").contains("Add to Watchlist").click()
+        cy.get("#message").should("contain", "the movie has been added to your watchlist")
+        
+      })
+
+    })
+
+
+
+  });
+
+});
---
+++ src/component/Auth.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import { storeAuthCredentials } from "../modules/auth";
+import AuthForm from "./AuthForm ";
+ 
+class Auth extends Component {
+    state = {
+        renderForm: false 
+    }
+    
+}
