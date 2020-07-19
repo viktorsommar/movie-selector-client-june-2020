a6641f95fd3635db91648c1e371ddaf0683b96d4
Sima <sima.boreiri@gmail.com>
2020-07-19 11:24:54 +0200

    remove Grid from authform

--- src/component/AuthForm.jsx
+++ src/component/AuthForm.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Form, Segment, Menu, Grid, Checkbox } from "semantic-ui-react";
+import { Button, Form, Segment, Menu, Checkbox } from "semantic-ui-react";
 
 const AuthForm = (props) => {
   return (
