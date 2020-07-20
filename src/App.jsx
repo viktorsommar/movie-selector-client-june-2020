1eeef196c708bf54a8b00072e9e390fcfe14a622
viktor_sommar <vsommarbaillot@gmail.com>
2020-07-20 18:35:36 +0200

    Fixing code to work with backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Movie from "./component/movie/Movie";
+import Movie from "./component/Movie";
 import Auth from "./component/Auth";
 
 class App extends Component {
